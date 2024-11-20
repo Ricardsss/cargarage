823cc2ee0c8f2a53349918b0d7c5ef61ca15b7f7
Ricardo <ricardo_slb_brites@live.ca>
2024-11-20 18:58:32 -0500

    Changed settings

--- cargarage/settings.py
+++ cargarage/settings.py
@@ -159,5 +159,3 @@ STORAGES = {
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
-
-CSRF_TRUSTED_ORIGINS = ["https://cargarage-aaaefeb42317.herokuapp.com"]
